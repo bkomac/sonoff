60eb5bb99db17bda69fbe4351896b3929473713f
bkomac <borut@komac.si>
2018-01-02 14:08:08 +0100

    added extra endpoints

--- src/sonoff/sonoff.ino
+++ src/sonoff/sonoff.ino
@@ -4,7 +4,7 @@ Espiot espiot;
 
 
 // APP
-String appV = "1.0.1";
+String appV = "1.0.2";
 String SENSOR1 = "RELEY"; // BMP180, HTU21, DHT11
 
 #define RELEY 12
@@ -15,17 +15,38 @@ String SENSOR1 = "RELEY"; // BMP180, HTU21, DHT11
 
 void setup() { //------------------------------------------------
   Serial.begin(115200);
-  Serial.println("Setup ...");
+  Serial.println("Sonoff v." + String(appV));
+
+  pinMode(RELEY, OUTPUT);
+  pinMode(BUILTINLED, OUTPUT);
+
   delay(300);
 
   espiot.init(appV);
   espiot.SENSOR = SENSOR1;
 
-  pinMode(RELEY, OUTPUT);
-  pinMode(BUILTINLED, OUTPUT);
+
 
   digitalWrite(RELEY, LOW);
   digitalWrite(BUILTINLED, HIGH);
+
+  espiot.server.on("/switch", HTTP_GET, []() {
+    espiot.blink();
+    Serial.println("/switch ...");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
+
+    root["GET State"] = "/switch/state";
+    root["ON"] = "/switch/on";
+    root["OFF"] = "/switch/off";
+
+    String content;
+    root.printTo(content);
+    espiot.server.send(200, "application/json", content);
+  });
 
   espiot.server.on("/switch/on", HTTP_GET, []() {
     espiot.blink();
@@ -33,6 +54,8 @@ void setup() { //------------------------------------------------
     DynamicJsonBuffer jsonBuffer;
     JsonObject &root = jsonBuffer.createObject();
 
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
     root["state"] = "on";
 
     digitalWrite(RELEY, HIGH);
@@ -49,10 +72,38 @@ void setup() { //------------------------------------------------
     DynamicJsonBuffer jsonBuffer;
     JsonObject &root = jsonBuffer.createObject();
 
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
     root["state"] = "off";
 
     digitalWrite(RELEY, LOW);
     digitalWrite(BUILTINLED, HIGH);
+
+    String content;
+    root.printTo(content);
+    espiot.server.send(200, "application/json", content);
+  });
+
+  espiot.server.on("/switch/toggle", HTTP_GET, []() {
+    espiot.blink();
+    Serial.println("/switch/toggle ...");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject &root = jsonBuffer.createObject();
+
+    root["id"] = espiot.getDeviceId();
+    root["name"] = espiot.deviceName;
+
+    if (digitalRead(RELEY) == LOW) {
+      Serial.println("state: on");
+      digitalWrite(RELEY, HIGH);
+      digitalWrite(BUILTINLED, LOW);
+      root["state"] = "on";
+    } else {
+      Serial.println("state: off");
+      digitalWrite(RELEY, LOW);
+      digitalWrite(BUILTINLED, HIGH);
+      root["state"] = "off";
+    }
 
     String content;
     root.printTo(content);
@@ -68,10 +119,10 @@ void setup() { //------------------------------------------------
     root["id"] = espiot.getDeviceId();
     root["name"] = espiot.deviceName;
 
-    if (RELEY == LOW)
+    if (digitalRead(RELEY) == LOW)
       root["state"] = "off";
     else
-      root["state"] = "onf";
+      root["state"] = "on";
 
     String content;
     root.printTo(content);
